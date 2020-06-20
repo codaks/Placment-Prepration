a428717bbe890397ef795bd2358254b684df1fe5
codaks <chitranshkulshresgtha.cs@geu.ac.in>
2020-06-20 11:21:38 +0530

    Game Theory Nim-Sum

--- GameTheory_Intro.cpp
+++ GameTheory_Intro.cpp
@@ -1,5 +1,4 @@
-/* A C++ program to implement Game of Nim. The program 
-assumes that both players are playing optimally */
+//THIS code is given by::----   https://www.geeksforgeeks.org/combinatorial-game-theory-set-2-game-nim/?ref=rp
 #include <iostream> 
 #include <math.h> 
 using namespace std; 
@@ -7,27 +6,14 @@ using namespace std;
 #define COMPUTER 1 
 #define HUMAN 2 
 
-/* A Structure to hold the two parameters of a move 
 
-A move has two parameters- 
-1) pile_index = The index of pile from which stone is 
-					going to be removed 
-2) stones_removed = Number of stones removed from the 
-						pile indexed = pile_index */
 struct move 
 { 
 	int pile_index; 
 	int stones_removed; 
 }; 
 
-/* 
-piles[] -> Array having the initial count of stones/coins 
-			in each piles before the game has started. 
-n	 -> Number of piles 
 
-The piles[] are having 0-based indexing*/
-
-// A C function to output the current game state. 
 void showPiles (int piles[], int n) 
 { 
 	int i; 
@@ -38,8 +24,7 @@ void showPiles (int piles[], int n)
 	return; 
 } 
 
-// A C function that returns True if game has ended and 
-// False if game is not yet over 
+
 bool gameOver(int piles[], int n) 
 { 
 	int i; 
@@ -50,7 +35,7 @@ bool gameOver(int piles[], int n)
 	return (true); 
 } 
 
-// A C function to declare the winner of the game 
+
 void declareWinner(int whoseTurn) 
 { 
 	if (whoseTurn == COMPUTER) 
@@ -60,8 +45,7 @@ void declareWinner(int whoseTurn)
 	return; 
 } 
 
-// A C function to calculate the Nim-Sum at any point 
-// of the game. 
+
 int calculateNimSum(int piles[], int n) 
 { 
 	int i, nimsum = piles[0]; 
@@ -70,20 +54,17 @@ int calculateNimSum(int piles[], int n)
 	return(nimsum); 
 } 
 
-// A C function to make moves of the Nim Game 
+
 void makeMove(int piles[], int n, struct move * moves) 
 { 
 	int i, nim_sum = calculateNimSum(piles, n); 
 
-	// The player having the current turn is on a winning 
-	// position. So he/she/it play optimally and tries to make 
-	// Nim-Sum as 0 
+
 	if (nim_sum != 0) 
 	{ 
 		for (i=0; i<n; i++) 
 		{ 
-			// If this is not an illegal move 
-			// then make this move. 
+             
 			if ((piles[i] ^ nim_sum) < piles[i]) 
 			{ 
 				(*moves).pile_index = i; 
@@ -94,23 +75,10 @@ void makeMove(int piles[], int n, struct move * moves)
 			} 
 		} 
 	} 
-
-	// The player having the current turn is on losing 
-	// position, so he/she/it can only wait for the opponent 
-	// to make a mistake(which doesn't happen in this program 
-	// as both players are playing optimally). He randomly 
-	// choose a non-empty pile and randomly removes few stones 
-	// from it. If the opponent doesn't make a mistake,then it 
-	// doesn't matter which pile this player chooses, as he is 
-	// destined to lose this game. 
-
-	// If you want to input yourself then remove the rand() 
-	// functions and modify the code to take inputs. 
-	// But remember, you still won't be able to change your 
-	// fate/prediction. 
+    
 	else
 	{ 
-		// Create an array to hold indices of non-empty piles 
+        
 		int non_zero_indices[n], count; 
 
 		for (i=0, count=0; i<n; i++) 
@@ -129,7 +97,7 @@ void makeMove(int piles[], int n, struct move * moves)
 	return; 
 } 
 
-// A C function to play the Game of Nim 
+
 void playGame(int piles[], int n, int whoseTurn) 
 { 
 	cout <<"\nGAME STARTS\n\n"; 
@@ -183,35 +151,17 @@ void knowWinnerBeforePlaying(int piles[], int n,
 	return; 
 } 
 
-// Driver program to test above functions 
+
 int main() 
 { 
-	// Test Case 1 
+     
 	int piles[] = {3, 4, 5}; 
 	int n = sizeof(piles)/sizeof(piles[0]); 
 
-	// We will predict the results before playing 
-	// The COMPUTER starts first 
+    
 	knowWinnerBeforePlaying(piles, n, COMPUTER); 
 
-	// Let us play the game with COMPUTER starting first 
-	// and check whether our prediction was right or not 
+
 	playGame(piles, n, COMPUTER); 
-
-	/* 
-	Test Case 2 
-	int piles[] = {3, 4, 7}; 
-	int n = sizeof(piles)/sizeof(piles[0]); 
-
-	// We will predict the results before playing 
-	// The HUMAN(You) starts first 
-	knowWinnerBeforePlaying (piles, n, COMPUTER); 
-
-	// Let us play the game with COMPUTER starting first 
-	// and check whether our prediction was right or not 
-	playGame (piles, n, HUMAN); */
-
 	return(0); 
 } 
-
-// This code is contributed by shivanisinghss2110 
